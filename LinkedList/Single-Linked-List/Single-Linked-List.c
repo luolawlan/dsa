f06a435406aebe06de31565e53f7e89c61a5b828
luolawlan <luolawlan@outlook.com>
2023-12-25 09:31:22 +0800

    data stucture

---
+++ LinkedList/Single-Linked-List/Single-Linked-List.c
@@ -0,0 +1,24 @@
+
+struct linkedNode;
+LiniedNode* createList(void);
+void insetern();
+
+struct LinkedNode {
+	int data;
+	struct *LinkedNode next;
+};
+
+LinkedNode* createList(void){
+	LinkedNode *head; 	
+	(*head).data = NULL;
+
+	return head;
+}
+
+void insetern(){
+
+}
+
+int main(void){
+	return 0;
+}
